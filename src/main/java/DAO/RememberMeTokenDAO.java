c48a2b5e4966f1357e52a18c96d88496367c5ec4
bnguy80 <80481071+bnguy80@users.noreply.github.com>
2023-08-23 21:19:28 -0400

    Create a method to generate and insert token for user

--- src/main/java/DAO/RememberMeTokenDAO.java
+++ src/main/java/DAO/RememberMeTokenDAO.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import model.RememberMeToken;
 
 import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import java.util.Base64;
 
 import java.sql.PreparedStatement;
@@ -12,7 +13,7 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 
 public class RememberMeTokenDAO {
-    private static void createNewTokenForUser(int userId, String username, String token, Timestamp expiresAt) {
+    private static void insertNewTokenForUser(int userId, String username, String token, Timestamp expiresAt) {
         try {
             String sql = "INSERT INTO remember_me_tokens (User_ID, User_Name, Token, Expires_At) VALUES (?, ?, ?, ?)";
             PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
@@ -27,6 +28,11 @@ public class RememberMeTokenDAO {
         }
     }
     // Create a new method called generateAndInsertTokenForUser to generate and insert a new token into the database for a given user
+    public static void generateAndInsertTokenForUser(int userId, String username) {
+        String token = generateRandomToken();
+        Timestamp expiresAt = Timestamp.valueOf(LocalDateTime.now().plusMinutes(1));
+        insertNewTokenForUser(userId, username, token, expiresAt);
+    }
     // Create a new method to generateRandomToken
     private static String generateRandomToken() {
         SecureRandom secureRandom = new SecureRandom();
